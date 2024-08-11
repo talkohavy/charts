a0ebf76ec6401f4db2de0f8b10c15d5abc1ddb3e
Tal Kohavy <talkohavy@gmail.com>
2024-08-11 10:29:54 +0300

    bundle looks much nicer now

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { resolve } from 'path';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { glob } from 'glob';
 import { defineConfig } from 'vite';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
   build: {
     copyPublicDir: false,
     lib: {
@@ -12,6 +15,22 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: ['lib/**/*.d.ts', 'lib/**/*.stories.tsx'],
+          })
+          .map((file) => [
+            // The name of the entry point lib/nested/foo.ts becomes nested/foo
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
     },
   },
 });
