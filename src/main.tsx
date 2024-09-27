94ec4182d43f6233e59923a764db43a849a75020
Tal Kohavy <talkohavy@gmail.com>
2024-09-27 10:28:30 +0300

    updated the main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import DarkThemeProvider from './providers/DarkThemeProvider';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+function Client() {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <DarkThemeProvider>
+          <App />
+        </DarkThemeProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
+
+const rootElement = document.getElementById('root')!;
+
+createRoot(rootElement).render(<Client />);
