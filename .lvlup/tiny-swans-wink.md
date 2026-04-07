1f6df63beb43950dbb75b6e67ac7b3d8f4e22379
talkohavy <talkohavy@gmail.com>
2026-04-07 09:04:46 +0300

    docs(lvlup): Export chart-related types from package.

---
+++ .lvlup/tiny-swans-wink.md
@@ -0,0 +1,5 @@
+---
+"@talkohavy/charts": minor
+---
+
+Export chart-related types from package.
--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,11 @@
 export { default as BarChart } from './components/BarChart';
 export { default as LineChart } from './components/LineChart';
 export { default as PieChart } from './components/PieChart';
+
+// constants
+export * from './components/logic/constants';
+
+// types
+export type * from './components/types';
+export type * from './components/logic/constants';
+export type * from './components/PieChart/types';
