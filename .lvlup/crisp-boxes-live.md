2693d376048c9f87bdee53dfa6adaef1e9de5e7d
talkohavy <talkohavy@gmail.com>
2026-04-07 23:58:57 +0300

    docs(lvlup): Added `recharts` as an external module. It is now not bundled as part of the package.

---
+++ .lvlup/crisp-boxes-live.md
@@ -0,0 +1,5 @@
+---
+"@talkohavy/charts": patch
+---
+
+Added `recharts` as an external module. It is now not bundled as part of the package.
--- vite.config.package.ts
+++ vite.config.package.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      external: ['react', 'react/jsx-runtime', 'recharts'],
       input: Object.fromEntries(
         glob
           .sync('lib/**/*.{ts,tsx}', {
