3ce77938fa9c85e9a5a1f939888dcf926e18efe0
talkohavy <talkohavy@gmail.com>
2026-04-07 12:47:57 +0300

    main background

--- src/components/Layout/Main/Main.tsx
+++ src/components/Layout/Main/Main.tsx
@@ -4,7 +4,7 @@ export default function Main(props: PropsWithChildren) {
   const { children } = props;
 
   return (
-    <main className='flex size-full items-center justify-between bg-white dark:bg-slate-900 dark:text-white'>
+    <main className='flex size-full items-center justify-between bg-white dark:bg-slate-950 dark:text-white'>
       {children}
     </main>
   );
